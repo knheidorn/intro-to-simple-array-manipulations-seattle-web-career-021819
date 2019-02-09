d8e5305ad319894fee7a863dbd1d2a8bc69c30db
Kim Heidorn <heidorkn@gmail.com>
2019-02-09 16:30:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,13 @@
 def using_push(array, element)
   array.push(element)
 end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+ array_2 = array.pop
+ array_2
+end
+
